b92a05d818298f0ab840a51bba77f198c5fdd849
Gleb <gleb.geracimenko@gmail.com>
2017-05-19 14:10:41 +0300

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,8 @@ public class Main {
     public static void main(String[] args) {
         TestTask testTask = new TestTask(1000000);
         System.out.println("Rectangles is created!");
-        List<Point> points = testTask.generatePoints(1000000, 1000);
+        List<Point> points = testTask.generatePoints(1000000, 7777);
         System.out.println("Pints is created!");
         testTask.verificationPoints(points);
     }
-
 }
--- src/main/java/TestTask.java
+++ src/main/java/TestTask.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by gleb on 17.05.17.
@@ -15,7 +17,7 @@ public class TestTask {
         rectangleList = new ArrayList<>(size);
         int x = 0;
         int y = 0;
-        int value = 100;
+        int value = 1000;
         int width = random(value) +10;
         int height = random(value) +10;
         for (int i = 0; i < size; i++) {
@@ -30,26 +32,38 @@ public class TestTask {
     }
 
     public void verificationPoints(List<Point> points) {
-        Iterator<Point> iterator;
         Collections.sort(points, (o1, o2) -> o1.x == o2.x ? o1.y - o2.y : o1.x - o2.x);
-        int count = 0;
-        for (Rectangle rectangle : rectangleList) {
-            iterator = points.iterator();
-            while (iterator.hasNext()) {
-                Point point = iterator.next();
-
-                if (rectangle.x > point.x || (rectangle.x + rectangle.width) < point.x &&
-                        rectangle.y > point.y || (rectangle.y + rectangle.height) < point.y) {
-                    break;
+        System.out.println("Pints sorted!");
+        int countPoint = 0;
+        int countRect = 0;
+        Map<Rectangle, List<Point>> result = new HashMap<>();
+        while (countRect < rectangleList.size() && countPoint < points.size()) {
+            Rectangle rectangle = rectangleList.get(countRect);
+            Point point = points.get(countPoint);
+            if (rectangle.x > point.x || (rectangle.x + rectangle.width) < point.x &&
+                    rectangle.y > point.y || (rectangle.y + rectangle.height) < point.y) {
+                countRect++;
+            } else {
+                List<Point> pointList = result.get(rectangle);
+                if (pointList == null) {
+                    pointList = new ArrayList<>();
+                    result.put(rectangle, pointList);
                 }
-                if (rectangle.contains(point)) {
-                    iterator.remove();
-                }
+                pointList.add(point);
+                countPoint++;
             }
-            System.out.println(points.size() + " rectangle size = " + ++count);
         }
-        System.out.println("End! Points size = " + points.size());
+        for (Rectangle rectangle : result.keySet()) {
+            List<Point> points1 = result.get(rectangle);
+            if (points1.size() < 10) {
+                System.out.println(rectangle + " => " + points1);
+            } else {
+                System.out.println(rectangle + " => " + points1.get(0) + "  --  " + points1.get(points1.size() -1));
+            }
+            System.out.println();
+        }
     }
+
 
     public List<Point> generatePoints(int size, int value) {
         List<Point> points = new ArrayList<>(size);
@@ -60,6 +74,6 @@ public class TestTask {
     }
 
     private int random(int value) {
-        return (int)(Math.random() * value);
+        return (int) (Math.random() * value);
     }
 }
